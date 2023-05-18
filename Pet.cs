29d3c0d156eda271b50d19cc8a1dfdc48ba4468a
Thomasgetacademy <thomasg@getacademy.no>
2023-05-18 09:09:57 +0200

    Add project files.

---
+++ Pet.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata;
+
+namespace Ukesoppgave_3;
+
+public class Pet
+{
+    private string _name;
+    private int _age;
+    private string _type;
+
+    public Pet(string name, int age, string type)
+    {
+        _name = name;
+        _age = age;
+        _type = type;
+    }
+
+    public void Feed(string food)
+    {
+        Console.WriteLine($"Thank you for the food, i do like {food} but prefer something else");
+    }
+    public void Feed(string food, bool isFavoriteFood)
+    {
+        Console.WriteLine($"Thank you for the food, {food} is my favorite!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ukesoppgave_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cat = new Pet("Tom", 2, "Cat");
+
+            cat.Feed("Tuna");
+            cat.Feed("Pizza", true);
+            Console.ReadKey();
+        }
+    }
+}
